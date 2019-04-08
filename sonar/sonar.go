a5f40454467ba1b295f1c0ca0da8d5712e9ecd17
Eduard Laur <eduard.laur@endava.com>
2019-04-08 17:14:07 +0300

    Adjustments

--- sonar/sonar.go
+++ sonar/sonar.go
@@ -6,27 +6,57 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"time"
 
 	b64 "encoding/base64"
 
+	client "github.com/influxdata/influxdb1-client"
 	ini "gopkg.in/ini.v1"
 )
 
+// Sonar represents response object from sonarqube rest api.
 type Sonar struct {
 	Component struct {
 		Key      string
 		Name     string
 		Measures []Measure
+		Type     string
 	}
 }
 
+// Measure represents a part of respose, defined in metricKeys
 type Measure struct {
 	Metric string
 	Value  string
 }
 
-func GetMeasures(url, token string, ch chan Sonar) {
+//Converts sonar api response strings to float
+func StringToFloat(metric, value string) float64 {
+	var alert = map[string]float64{
+		"OK":    1,
+		"WARN":  0.5,
+		"ERROR": 0,
+	}
+
+	var val float64
+	var err error
+
+	if metric != "alert_status" {
+		val, err = strconv.ParseFloat(value, 64)
+		if err != nil {
+		}
+	} else {
+		val = alert[value]
+	}
+
+	return val
+}
+
+// GetMeasures sends mapped Sonar object from response to channel
+func GetMeasures(url, componentType, token string, ch chan Sonar) {
 
 	token = "Basic " + token
 	req, err := http.NewRequest("GET", url, nil)
@@ -46,8 +76,58 @@ func GetMeasures(url, token string, ch chan Sonar) {
 
 	json.Unmarshal(body, &s)
 
+	s.Component.Type = componentType
+
 	ch <- s
 
+}
+
+// ClientWrite inserts measures into influx database
+func ClientWrite(databaseURL, databse string, projects []Sonar) {
+	host, err := url.Parse(fmt.Sprintf("http://%s", databaseURL))
+	if err != nil {
+		log.Print(err)
+	}
+
+	con, err := client.NewClient(client.Config{URL: *host, Timeout: 300 * time.Second})
+	if err != nil {
+		log.Print(err)
+	}
+
+	var (
+		pts = []client.Point{}
+	)
+
+	for _, p := range projects {
+		key := p.Component.Key
+		cType := p.Component.Type
+		for _, m := range p.Component.Measures {
+			metric := m.Metric
+			value := StringToFloat(metric, m.Value)
+			point := client.Point{
+				Measurement: metric,
+				Tags: map[string]string{
+					"project_key":    key,
+					"component_type": cType,
+				},
+				Fields: map[string]interface{}{
+					"val": value,
+				},
+				Time:      time.Now(),
+				Precision: "n",
+			}
+			pts = append(pts, point)
+		}
+	}
+
+	bps := client.BatchPoints{
+		Points:   pts,
+		Database: databse,
+	}
+	_, err = con.Write(bps)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -56,9 +136,14 @@ func main() {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
 	}
+	var divided [][]string
 
+	start := time.Now()
 	repos := cfg.SectionStrings()[1:]
+	chunkSize := cfg.Section("").Key("chunks").MustInt(9999)
 	sonarURL := cfg.Section("").Key("sonarUrl").String()
+	database := cfg.Section("").Key("dbName").String()
+	databaseURL := cfg.Section("").Key("dbHost").String() + ":" + cfg.Section("").Key("dbPort").String()
 	metricKeys := cfg.Section("").Key("metricKeys").String()
 	apiPath := "/api/measures/component?component="
 	token := cfg.Section("").Key("token").String()
@@ -66,10 +151,27 @@ func main() {
 
 	ch := make(chan Sonar)
 
-	for r := range repos {
-		key := cfg.Section(repos[r]).Key("key").String()
-		url := sonarURL + apiPath + key + "&metricKeys=" + metricKeys
-		go GetMeasures(url, tokenEnc, ch)
+	// for r := range repos {
+
+	// }
+
+	for i := 0; i < len(repos); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(repos) {
+			end = len(repos)
+		}
+
+		divided = append(divided, repos[i:end])
+	}
+
+	for _, chunk := range divided {
+		for _, repo := range chunk {
+			key := cfg.Section(repo).Key("key").String()
+			componentType := cfg.Section(repo).Key("type").String()
+			url := sonarURL + apiPath + key + "&metricKeys=" + metricKeys
+			go GetMeasures(url, componentType, tokenEnc, ch)
+		}
 	}
 
 	var projects []Sonar
@@ -78,6 +180,9 @@ func main() {
 		projects = append(projects, <-ch)
 	}
 
-	fmt.Print(projects)
+	ClientWrite(databaseURL, database, projects)
+
+	elapsed := time.Since(start)
+	log.Printf("Took %s", elapsed)
 
 }
