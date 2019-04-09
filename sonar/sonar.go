6a4d6c5400cb2ca77dde14063ba5d07a9f5a7c76
Eduard Laur <eduard.laur@endava.com>
2019-04-09 15:25:51 +0300

    Add jenkins integration

--- sonar/sonar.go
+++ sonar/sonar.go
@@ -33,7 +33,15 @@ type Measure struct {
 	Value  string
 }
 
-//Converts sonar api response strings to float
+// JenkinsJob represents response object from jenkins rest api.
+type JenkinsJob struct {
+	Result    string
+	Timestamp int64
+	URL       string
+	Key       string
+}
+
+// StringToFloat converts sonar api response strings to float
 func StringToFloat(metric, value string) float64 {
 	var alert = map[string]float64{
 		"OK":    1,
@@ -41,18 +49,34 @@ func StringToFloat(metric, value string) float64 {
 		"ERROR": 0,
 	}
 
+	var status = map[string]float64{
+		"SUCCESS":  1,
+		"UNSTABLE": 0.5,
+		"ERROR":    0,
+	}
+
 	var val float64
 	var err error
 
-	if metric != "alert_status" {
+	if metric == "alert_status" {
+		val = alert[value]
+	} else if metric == "status" {
+		val = status[value]
+	} else {
 		val, err = strconv.ParseFloat(value, 64)
 		if err != nil {
+			log.Printf("Unable to convert string to float")
 		}
-	} else {
-		val = alert[value]
 	}
 
 	return val
+}
+
+// SinceJob returns number of hours since given unix timestamp (13 digits) from jenkins response
+func SinceJob(t int64) float64 {
+	UTCfromUnixNano := time.Unix(int64(t/1000), 0)
+	duration := time.Since(UTCfromUnixNano)
+	return duration.Hours()
 }
 
 // GetMeasures sends mapped Sonar object from response to channel
@@ -82,8 +106,34 @@ func GetMeasures(url, componentType, token string, ch chan Sonar) {
 
 }
 
+// GetJob sends mapped JenkinsJob object from response to channel
+func GetJob(repo, url, token string, ch chan JenkinsJob) {
+
+	token = "Basic " + token
+	req, err := http.NewRequest("GET", url+"/lastBuild/api/json?tree=result,timestamp,url", nil)
+
+	req.Header.Add("Authorization", token)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var j JenkinsJob
+
+	json.Unmarshal(body, &j)
+	j.Key = repo
+
+	ch <- j
+
+}
+
 // ClientWrite inserts measures into influx database
-func ClientWrite(databaseURL, databse string, projects []Sonar) {
+func ClientWrite(databaseURL, databse string, projects []Sonar, jobs []JenkinsJob) {
 	host, err := url.Parse(fmt.Sprintf("http://%s", databaseURL))
 	if err != nil {
 		log.Print(err)
@@ -120,6 +170,51 @@ func ClientWrite(databaseURL, databse string, projects []Sonar) {
 		}
 	}
 
+	for _, j := range jobs {
+		key := j.Key
+		statusValue := StringToFloat("status", j.Result)
+		todayValue := SinceJob(j.Timestamp)
+		urlValue := j.URL
+		status := client.Point{
+			Measurement: "jenkins_status",
+			Tags: map[string]string{
+				"project_key": key,
+			},
+			Fields: map[string]interface{}{
+				"val": statusValue,
+			},
+			Time:      time.Now(),
+			Precision: "n",
+		}
+
+		url := client.Point{
+			Measurement: "jenkins_url",
+			Tags: map[string]string{
+				"project_key": key,
+			},
+			Fields: map[string]interface{}{
+				"val": urlValue,
+			},
+			Time:      time.Now(),
+			Precision: "n",
+		}
+
+		since := client.Point{
+			Measurement: "jenkins_since",
+			Tags: map[string]string{
+				"project_key": key,
+			},
+			Fields: map[string]interface{}{
+				"val": todayValue,
+			},
+			Time:      time.Now(),
+			Precision: "n",
+		}
+
+		pts = append(pts, status, url, since)
+
+	}
+
 	bps := client.BatchPoints{
 		Points:   pts,
 		Database: databse,
@@ -131,7 +226,7 @@ func ClientWrite(databaseURL, databse string, projects []Sonar) {
 }
 
 func main() {
-	cfg, err := ini.Load("config.ini")
+	cfg, err := ini.Load("../config.ini")
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
@@ -148,12 +243,12 @@ func main() {
 	apiPath := "/api/measures/component?component="
 	token := cfg.Section("").Key("token").String()
 	tokenEnc := b64.StdEncoding.EncodeToString([]byte(token + ":"))
+	jenkinsToken := cfg.Section("").Key("jenkins_token").String()
+	jenkinsUser := cfg.Section("").Key("jenkins_user").String()
+	jenkinsTokenEnc := b64.StdEncoding.EncodeToString([]byte(jenkinsUser + ":" + jenkinsToken))
 
 	ch := make(chan Sonar)
-
-	// for r := range repos {
-
-	// }
+	jch := make(chan JenkinsJob)
 
 	for i := 0; i < len(repos); i += chunkSize {
 		end := i + chunkSize
@@ -170,17 +265,21 @@ func main() {
 			key := cfg.Section(repo).Key("key").String()
 			componentType := cfg.Section(repo).Key("type").String()
 			url := sonarURL + apiPath + key + "&metricKeys=" + metricKeys
+			jenkinsURL := cfg.Section(repo).Key("job").String()
 			go GetMeasures(url, componentType, tokenEnc, ch)
+			go GetJob(key, jenkinsURL, jenkinsTokenEnc, jch)
 		}
 	}
 
 	var projects []Sonar
+	var jobs []JenkinsJob
 
 	for range repos {
 		projects = append(projects, <-ch)
+		jobs = append(jobs, <-jch)
 	}
 
-	ClientWrite(databaseURL, database, projects)
+	ClientWrite(databaseURL, database, projects, jobs)
 
 	elapsed := time.Since(start)
 	log.Printf("Took %s", elapsed)
